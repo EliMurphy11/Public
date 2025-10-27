72fa9f61794b9f00880ef3ba54e9b1c742c458d9
Eli Murphy <elijahdavidmurphy@gmail.com>
2025-10-27 10:23:38 -0500

    chapter_3

---
+++ python_work/chapter_2/inclasss.py/chapter__2/chapter__3/3.2.py
@@ -0,0 +1,4 @@
+names = ("eli", "asher", "paxon")
+print("hello " + names[0])
+print("hello " + names[1])
+print("hello " + names[2])
---
+++ python_work/chapter_2/inclasss.py/chapter__2/chapter__3/3.8.py
@@ -0,0 +1,8 @@
+places_tosee = ("japan", "greece", "italy", "australia", "switzerland")
+print("I would like to visit " + places_tosee[0].title() + ".")
+print("sorted list: " + str(sorted(places_tosee)))
+print("original list: " + str(places_tosee))
+print("reversed list: " + str(sorted(places_tosee, reverse=True)))
+print("original list again: " + str(places_tosee))
+places_tosee = ("japan", "greece", "italy", "australia", "switzerland")
+
